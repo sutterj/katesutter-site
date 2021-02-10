a287dcd89f0c5738b923361a85020f456ffaec57
sutterj <jesutter23@me.com>
2021-02-09 20:59:31 -0500

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,14 +4,14 @@ import { Link } from "gatsby"
 import { RiHeart2Line } from "react-icons/ri";
 
 const Footer = () => (
-  <footer 
+  <footer
     className="site-footer"
     sx={{
       bg: 'primary'
     }}
   >
     <div className="container">
-      <p>A GatsbyJS Starter for Netlify CMS, Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="/">Stackrole.com</Link></p>
+      <p>A bio / portfolio site for artist, Kate Sutter</p>
     </div>
   </footer>
 )
