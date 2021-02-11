fc88127abe15ded83d655d9239226c3f59aeb204
sutterj <jesutter23@me.com>
2021-02-11 17:17:17 -0500

    Update thanks.js

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -15,9 +15,9 @@ const Thanks = () => (
         fontSize: "128px",
         color: "var(--primary-color)"
       }}/>
-      <h1>Got your message</h1>
-      <p>Thank you for getting in touch us. We will get back to you shortly.</p>
-      <Link to="/" className="button"><RiArrowLeftSLine className="icon -left"/>Lets go back to Homepage</Link>
+      <h1>Message Received</h1>
+      <p>Thank you for getting in touch. I will get back to you shortly.</p>
+      <Link to="/" className="button"><RiArrowLeftSLine className="icon -left"/>Back to Homepage</Link>
     </div>
 
   </Layout>
